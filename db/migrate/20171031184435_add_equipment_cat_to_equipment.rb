8f9825b37ec98ae66cb50492b8a478b27165fecb
ThePudgyPlatypi <cstehm@comcast.net>
2017-11-07 21:44:27 -0500

    removed migrations

--- db/migrate/20171031184435_add_equipment_cat_to_equipment.rb
+++
@@ -1,5 +0,0 @@
-class AddEquipmentCatToEquipment < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :equipment, :equipment_cat, foreign_key: true
-  end
-end
--- db/migrate/20171031184557_create_equipments_exercises.rb
+++
@@ -1,8 +0,0 @@
-class CreateEquipmentsExercises < ActiveRecord::Migration[5.0]
-  def change
-    create_table :equipment_exercises do |t|
-      t.references :exercise, foreign_key: true
-      t.references :equipment, foreign_key: true
-    end
-  end
-end
